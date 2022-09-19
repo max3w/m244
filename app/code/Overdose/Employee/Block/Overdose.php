819bd1a0547ed8c37406a1e8553251bbb220552b
m.alekseenko <m.alekseenko@skywell.software>
2022-09-19 17:04:14 +0300

    The old way of adding content to a function in phtml

---
+++ app/code/Overdose/Employee/Block/Overdose.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Overdose\Employee\Block;
+
+class Overdose extends \Magento\Framework\View\Element\Template
+{
+    public function doSomethingImportant(): string
+    {
+        return ('I am an important text.');
+    }
+}
