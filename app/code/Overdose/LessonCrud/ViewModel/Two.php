56415b681a4cd9fe4de85c1af247c82e0081b9e6
m.alekseenko <m.alekseenko@skywell.software>
2022-11-10 18:37:16 +0200

    Add first save database

--- app/code/Overdose/LessonCrud/ViewModel/Two.php
+++ app/code/Overdose/LessonCrud/ViewModel/Two.php
@@ -4,10 +4,33 @@ namespace Overdose\LessonCrud\ViewModel;
 
 class Two implements \Magento\Framework\View\Element\Block\ArgumentInterface
 {
+    protected $friendsFactory;
+    protected $friendsResourceModel;
+    protected $friendsCollectionFactory;
+
+    public function __construct(
+        \Overdose\LessonCrud\Model\FriendsFactory $friendsFactory,
+        \Overdose\LessonCrud\Model\ResourceModel\Friends $friendsResourceModel,
+        \Overdose\LessonCrud\Model\ResourceModel\Collection\FriendsFactory $friendsCollectionFactory
+    ) {
+        $this->friendsFactory = $friendsFactory;
+        $this->friendsResourceModel = $friendsResourceModel;
+        $this->friendsCollectionFactory = $friendsCollectionFactory;
+    }
 
     public function iAmViewModel(): string
     {
         return ('I am Two view model');
     }
 
+    public function saveNewFriend($name, $age, $comment){
+       // 'Semen', '17', 'A cool guy'
+     $model = $this->friendsFactory->create();
+
+     $model->setData('name', $name)
+        ->setData('age', $age)
+        ->setData('comment', $comment);
+     //Save
+        $this->friendsResourceModel->save($model);
+}
 }
